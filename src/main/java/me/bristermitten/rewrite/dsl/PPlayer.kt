b55862b942332a19751578a61854960519d59215
knightzmc <alexljwood24@hotmail.co.uk>
2019-12-15 08:49:45 +0000

    Add sendMessage to PPlayer.kt

--- src/main/java/me/bristermitten/rewrite/dsl/PPlayer.kt
+++ src/main/java/me/bristermitten/rewrite/dsl/PPlayer.kt
@@ -4,7 +4,13 @@ import me.bristermitten.rewrite.dsl.core.LengthConfiguration
 import me.bristermitten.rewrite.dsl.core.PlumberEntity
 import org.bukkit.event.Cancellable
 import org.bukkit.event.player.PlayerEvent
+import kotlin.reflect.KClass
 
 interface PPlayer : PlumberEntity<PPlayer, PlayerActionFilter, PlayerReactorPicker> {
-    fun <T > blockEvent(clazz: Class<T>) : LengthConfiguration<PlayerActionFilter, PlayerReactorPicker> where T : PlayerEvent, T : Cancellable
+
+    fun <T> blockEvent(clazz: Class<T>): LengthConfiguration<PlayerActionFilter, PlayerReactorPicker> where T : PlayerEvent, T : Cancellable
+    fun <T> blockEvent(clazz: KClass<T>): LengthConfiguration<PlayerActionFilter, PlayerReactorPicker> where T : PlayerEvent, T : Cancellable
+
+    fun sendMessage(message: String)
+
 }
